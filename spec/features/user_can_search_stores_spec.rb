d990f825e5372b2e2edc28927282ff474b003be6
Aram Anderson <aram.j.anderson@gmail.com>
2017-11-01 10:11:02 -0600

    Search test passing

--- spec/features/user_can_search_stores_spec.rb
+++ spec/features/user_can_search_stores_spec.rb
@@ -3,17 +3,24 @@ require 'rails_helper'
 feature "As a user" do
   context "When I visit '/' and I fill in a search box with '80202' and click 'search'" do
     scenario "I should see 17 stores within 25 miles of 80202" do
-    visit "/"
-    fill_in 'search', with: "80202"
-    expect(current_path).to eq("/search/*")
+      VCR.use_cassette("best buy search") do
+        visit "/"
+        fill_in 'search', with: "80202"
+        click_on "Locate"
+        expect(current_path).to eq("/search")
 
-    expect(page).to have_content("17 Total Stores")
-    expect(".store".count).to eq(10)
+        expect(page).to have_content("17 Total Stores")
+        within(first(".store")) do
+          expect(page).to have_css(".name")
+          expect(page).to have_css(".city")
+          expect(page).to have_css(".distance")
+          expect(page).to have_css(".phone")
+          expect(page).to have_css(".type")
+        end
+      end
+    end
   end
 end
-end
-
-
 
 # And I should see exactly 10 results (There are 17 stores within 25 miles. We want to display 10 on this page and 7 on the next. Get the first page to work first from start to finish and worry about pagination later.)
 # And I should see the long name, city, distance, phone number and store type for each of the 10 results
