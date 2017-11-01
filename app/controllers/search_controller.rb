bd6aef6df9e4036f6b8bb8cc7bb55613b4ce965a
Aram Anderson <aram.j.anderson@gmail.com>
2017-11-01 11:39:34 -0600

    Working on refactoring API call to bestbuy

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,8 @@
 class SearchController < ApplicationController
   def index
-    response = Faraday.get( "https://api.bestbuy.com/v1/stores(area(#{params[:search]},25))?format=json&show=longName,storeType,city,distance,phone&pageSize=25&apiKey=#{ENV["BEST_BUY_API_KEY"]}").body
-    parsed_response = JSON.parse(response, symbolize_names: true)[:stores]
-    @stores = Store.make_stores(parsed_response)
+    # response = Faraday.get( "https://api.bestbuy.com/v1/stores(area(#{params[:search]},25))?format=json&show=longName,storeType,city,distance,phone&pageSize=25&apiKey=#{ENV["BEST_BUY_API_KEY"]}").body
+    # parsed_response = JSON.parse(response, symbolize_names: true)[:stores]
+    # @stores = Store.make_stores(parsed_response)
+    @stores = SearchService.new.find_by_zip(params[:search])
   end
 end
--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -4,7 +4,23 @@ class SearchService
     @conn = get_conn
   end
 
-private
+  def find_by_zip(zip_code)
+    raw_data = get_raw_data(zip_code)
+    make_stores(raw_data)
+  end
+
+  def get_raw_data(zip_code)
+    binding.pry
+    conn.get("/stores(area(#{zip_code},25))?format=json&show=longName,storeType,city,distance,phone&pageSize=25&apiKey=#{ENV['BEST_BUY_API_KEY']}").body
+  end
+
+  def make_stores(raw_data)
+    Store.make_stores(raw_data)
+  end
+
+  private
+
+  attr_reader :conn
 
   def get_conn
     Faraday.new(url: "https://api.bestbuy.com/v1") do |faraday|
