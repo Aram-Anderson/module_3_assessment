d64ac23436f40702ced7f066e1f93fd1e0a96f13
Aram Anderson <aram.j.anderson@gmail.com>
2017-11-01 11:03:42 -0600

    Working on API create action

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -10,4 +10,17 @@ class Api::V1::ItemsController < ApplicationController
    item = Item.find(params[:id])
    render json: item
  end
+
+ def create
+   binding.pry
+   if item = Item.create(item_params)
+     render json: :item, status: :success
+   end
+ end
+
+ private
+
+ def item_params
+   params.require(:item).permit(:name, :description, :image_url)
+ end
 end
--- spec/requests/api/v1/create_item_spec.rb
+++ spec/requests/api/v1/create_item_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
-feature "When I send a POST request to `/api/v1/items`" do
-  context "with a name, description, and image_url" do
-    scenario "I receive a 201 JSON  response if the record is successfully created" do
+context "When I send a POST request to `/api/v1/items`" do
+  describe "with a name, description, and image_url" do
+    it "I receive a 201 JSON  response if the record is successfully created" do
+
+      post 'api/v1/items?name=thing1&description=thingsandstuff&image_url=thing.png'
 
     end
-    scenario "And I receive a JSON response containing the id, name, description, and image_url but not the created_at or updated_at" do
-      
+    it "And I receive a JSON response containing the id, name, description, and image_url but not the created_at or updated_at" do
+
     end
   end
 end
