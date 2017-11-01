d476e843dffc75e90b7c0112cf48fade1f5cacce
Aram Anderson <aram.j.anderson@gmail.com>
2017-11-01 10:49:56 -0600

    One item spec passing

--- spec/requests/api/v1/one_item_spec.rb
+++ spec/requests/api/v1/one_item_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
-feature "When I send a GET request to `/api/v1/items/1`" do
-  context "I receive a 200 JSON response" do
-    scenario "And the page contains the id, name, description, and image_url but not the created_at or updated_at" do
-      
+context "When I send a GET request to `/api/v1/items/1`" do
+  describe "I receive a 200 JSON response" do
+    it "And the page contains the id, name, description, and image_url but not the created_at or updated_at" do
+      item1, item2 = create_list(:item, 2)
+      get "/api/v1/items/#{item1.id}"
+      item = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_success
+      expect(item[:name]).to eq(item1.name)
     end
   end
 end
